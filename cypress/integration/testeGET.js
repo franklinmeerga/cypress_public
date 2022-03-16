79142a3b897cc336a327bc300229be31e545676a
franklinmeerga <62896416+franklinmeerga@users.noreply.github.com>
2022-03-16 10:24:40 -0300

    Delete testeGET.js

--- cypress/integration/testeGET.js
+++
@@ -1,8 +0,0 @@
-describe('serch test cases', function(){
-    it('serch with results', function(){
-        //cy.visit('http://automationpractice.com/index.php');
-        cy.visit('https://simoncoll.myvtex.com/_v/segment/admin-login/v1/login?returnUrl=%2F%3F&__bindingAddress=www.simoncoll.com/es'); // serch the website
-        cy.get('#email').type('franklin.garcia@acct.global'); // put the email adress
-        cy.get('.vtex-button__label').click();
-    })
-})
